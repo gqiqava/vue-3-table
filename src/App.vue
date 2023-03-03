4ebe0de811f194f3360186d9346e50fd2a4a7ef2
Giorgi <giogioqiqava777@gmail.com>
2023-03-03 14:42:59 +0400

    users api

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,183 @@
 <template>
-  <TableComponent msg="You did it!" />
+  <TableComponent :data-table="inMeal" />
 </template>
 <script setup>
+import { ref } from 'vue'
 import TableComponent from './components/TableComponent.vue'
+
+let users = ref()
+
+fetch('https://dummyjson.com/users')
+  .then((res) => res.json())
+  .then((res) => (users.value = res.users))
+
+let inMeal = ref([
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ატრია',
+    price: 15,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-ცხარე', double_meat: true },
+    number: '599568686',
+    time: '11:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '1'
+  },
+  {
+    name: 'დავით',
+    order: 'საქონლის ატრია',
+    price: 12.55,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599500686',
+    time: '15:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '2'
+  },
+  {
+    name: 'ბექა',
+    order: 'ქათმის ატრია',
+    price: 26,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: true },
+    number: '599565586',
+    time: '20:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '3'
+  },
+  {
+    name: 'ლაზარე',
+    order: 'სქონლის ატრია',
+    price: 32,
+    category: { name: 'ბრინჯი', id: 2 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599568600',
+    time: '15:19',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '4'
+  },
+  {
+    name: 'ლაზარე',
+    order: 'სქონლის ატრია',
+    price: 33,
+    category: { name: 'ბრინჯი', id: 2 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599568600',
+    time: '15:19',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '4'
+  },
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ბრინჯი',
+    price: 15,
+    category: { name: 'ბრინჯი', id: 2 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: true },
+    number: '599568686',
+    time: '23:15',
+    comment: 'სტაფილოს მეტი რაა',
+    id: '42'
+  },
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ატრია',
+    price: 18,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599568686',
+    time: '17:15',
+    comment: 'სტაფილოს მეტი რაა',
+    id: '42'
+  },
+  {
+    name: 'ჯემო',
+    order: 'საქონლის ბრინჯი',
+    price: 17,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599567000',
+    time: '15:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '41'
+  },
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ატრია',
+    price: 15,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-ცხარე', double_meat: true },
+    number: '599568686',
+    time: '11:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '1'
+  },
+  {
+    name: 'თემო',
+    order: 'საქონლის ატრია',
+    price: 12.55,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '591943299',
+    time: '15:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '2'
+  },
+  {
+    name: 'ბექა',
+    order: 'ქათმის ატრია',
+    price: 26,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: true },
+    number: '599565586',
+    time: '20:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '3'
+  },
+  {
+    name: 'ლაზარე',
+    order: 'სქონლის ატრია',
+    price: 32,
+    category: { name: 'ბრინჯი', id: 2 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599568700',
+    time: '15:19',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '4'
+  },
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ბრინჯი',
+    price: 15,
+    category: { name: 'ბრინჯი', id: 2 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: true },
+    number: '599568686',
+    time: '23:15',
+    comment: 'სტაფილოს მეტი რაა',
+    id: '42'
+  },
+  {
+    name: 'გიორგი',
+    order: 'ქათმის ატრია',
+    price: 18,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599568626',
+    time: '17:15',
+    comment: 'სტაფილოს მეტი რაა',
+    id: '42'
+  },
+  {
+    name: 'ჯემო',
+    order: 'საქონლის ბრინჯი',
+    price: 17,
+    category: { name: 'ატრია', id: 1 },
+    details: { sauce: 'ტკბილ-მჟავე', double_meat: false },
+    number: '599567010',
+    time: '15:15',
+    comment: 'სტაფილოს გარეშე რაა',
+    id: '41'
+  }
+])
 </script>
 
 <style scoped></style>
--- src/components/TableComponent.vue
+++ src/components/TableComponent.vue
@@ -1,12 +1,193 @@
 <template>
-  {{ msg }}
+  <table class="table table-bordered container mt-5 border" style="border-spacing: 2">
+    <tr>
+      <th>
+        მომხმარებელი
+        <button type="button" @click="sortDataByName" class="btn btn-light ms-1 p-1 fw-bold">
+          ^
+        </button>
+      </th>
+      <th>
+        ნომერი
+        <button type="button" @click="sortDataByNumber" class="btn btn-light ms-1 p-1 fw-bold">
+          ^
+        </button>
+      </th>
+      <th>
+        შეკვეთა
+        <button type="button" @click="sortDataByOrder" class="btn btn-light ms-1 p-1 fw-bold">
+          ^
+        </button>
+      </th>
+      <th>
+        ფასი
+        <button type="button" @click="sortDataByPrice" class="btn btn-light ms-1 p-1 fw-bold">
+          ^
+        </button>
+      </th>
+      <th>დეტალები</th>
+      <th>
+        დრო
+        <button type="button" @click="sortDataByTime" class="btn btn-light ms-1 p-1 fw-bold">
+          ^
+        </button>
+      </th>
+    </tr>
+    <tr class="mb-5">
+      <th class="p-1">
+        <input
+          type="text"
+          class="form-control"
+          v-model="name"
+          placeholder="სახელი"
+          style="border: none"
+        />
+      </th>
+      <th>
+        <input
+          type="text"
+          class="form-control"
+          v-model="number"
+          placeholder="ნომერი"
+          style="border: none"
+        />
+      </th>
+      <th>
+        <input
+          type="text"
+          class="form-control"
+          v-model="order"
+          placeholder="შეკვეთა"
+          style="border: none"
+        />
+      </th>
+      <th>
+        <input
+          type="text"
+          class="form-control"
+          v-model="price"
+          placeholder="ფასი"
+          style="border: none"
+        />
+      </th>
+      <th>
+        <input
+          type="text"
+          class="form-control"
+          v-model="details"
+          placeholder="დეტალები"
+          style="border: none"
+        />
+      </th>
+      <th>
+        <input
+          type="text"
+          class="form-control"
+          v-model="time"
+          placeholder="დრო"
+          style="border: none"
+        />
+      </th>
+    </tr>
+    <tr v-for="(item, ind) in userTable" :key="ind" :class="{ darker: ind % 2 === 0 }">
+      <td class="ps-1">{{ item.name }}</td>
+      <td>{{ item.number }}</td>
+      <td>{{ item.order }}</td>
+      <td>{{ item.price }} ₾</td>
+      <td>
+        {{ item.details.sauce }},
+        <span v-if="item.details.double_meat">ორმაგი ხორცი</span>
+        <span v-else>სტანდარტული ხორცი</span>
+      </td>
+      <td>{{ item.time }}</td>
+    </tr>
+  </table>
+  <button class="btn btn-success fw-bold ms-5" @click="generateCSV()">Download XLSX</button>
 </template>
+
 <script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
+import { ref, watch } from 'vue'
+
+let csvString = ref()
+
+let name = ref('')
+let number = ref('')
+let order = ref('')
+let price = ref('')
+let details = ref('')
+let time = ref('')
+
+let inputArr = ref([name, number, order, price, details, time])
+
+const props = defineProps({
+  dataTable: Array
 })
+
+let compTable = ref(props.dataTable)
+let userTable = ref(props.dataTable)
+
+watch(
+  inputArr,
+  () => {
+    userTable.value = compTable.value.filter(
+      (e) =>
+        e.name.includes(name.value) &&
+        e.number.includes(number.value) &&
+        e.order.includes(order.value) &&
+        e.price.toString().includes(price.value) &&
+        e.time.includes(time.value)
+    )
+  },
+  { deep: true }
+)
+
+// Sorts
+
+const sortDataByName = () => {
+  userTable.value.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0))
+}
+
+const sortDataByTime = () => {
+  userTable.value.sort((a, b) => (a.time > b.time ? 1 : b.time > a.time ? -1 : 0))
+}
+
+const sortDataByOrder = () => {
+  userTable.value.sort((a, b) => (a.order > b.order ? 1 : b.order > a.order ? -1 : 0))
+}
+
+const sortDataByPrice = () => {
+  userTable.value.sort((a, b) => a.price - b.price)
+}
+
+const sortDataByNumber = () => {
+  userTable.value.sort((a, b) => a.number - b.number)
+}
+
+// Generate XLSX
+
+const generateCSV = () => {
+  csvString.value = [
+    ['Name', 'Number', 'Time', 'Order', 'Price', 'Category', 'Sauce', 'Double meat'],
+    ...userTable.value.map((item) => [
+      item.name,
+      item.number,
+      item.time,
+      item.order,
+      item.price,
+      item.category.name,
+      item.details.sauce,
+      item.details.double_meat
+    ])
+  ]
+    .map((e) => e.join(','))
+    .join('\n')
+
+  var hiddenElement = document.createElement('a')
+  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvString.value)
+  hiddenElement.target = '_blank'
+  hiddenElement.download = 'data.csv'
+  hiddenElement.click()
+}
 </script>
+
 <style scoped></style>
